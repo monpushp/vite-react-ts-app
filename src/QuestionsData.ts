2c25a8e70a4c8d5c87c5fcd7e07c8c402ba60bd1
PK <monpushp@gmail.com>
2023-11-26 21:28:43 +0530

    Changes to be committed: 	modified:   src/QuestionPage.tsx 	modified:   src/QuestionsData.ts     Page 166

--- src/QuestionsData.ts
+++ src/QuestionsData.ts
@@ -16,33 +16,33 @@ export interface AnswerData {
 const questions: QuestionData[] = [
   {
     questionId: 1,
-    title: 'Why should I learn TypeScript?',
+    title: "Why should I learn TypeScript?",
     content:
-      'TypeScript seems to be getting popular so I wondered whether it is worth my time learning it? What benefits does it give over JavaScript?',
-    userName: 'Bob',
+      "TypeScript seems to be getting popular so I wondered whether it is worth my time learning it? What benefits does it give over JavaScript?",
+    userName: "Bob",
     created: new Date(),
     answers: [
       {
         answerId: 1,
-        content: 'To catch problems earlier speeding up your developments',
-        userName: 'Jane',
+        content: "To catch problems earlier speeding up your developments",
+        userName: "Jane",
         created: new Date(),
       },
       {
         answerId: 2,
         content:
-          'So, that you can use the JavaScript features of tomorrow, today',
-        userName: 'Fred',
+          "So, that you can use the JavaScript features of tomorrow, today",
+        userName: "Fred",
         created: new Date(),
       },
     ],
   },
   {
     questionId: 2,
-    title: 'Which state management tool should I use?',
+    title: "Which state management tool should I use?",
     content:
-      'There seem to be a fair few state management tools around for React - React, Unstated, ... Which one should I use?',
-    userName: 'Bob',
+      "There seem to be a fair few state management tools around for React - React, Unstated, ... Which one should I use?",
+    userName: "Bob",
     created: new Date(),
     answers: [],
   },
@@ -51,6 +51,14 @@ const questions: QuestionData[] = [
 export const getUnansweredQuestions = async (): Promise<QuestionData[]> => {
   await wait(500);
   return questions.filter((q) => q.answers.length === 0);
+};
+
+export const getQuestion = async (
+  questionId: number
+): Promise<QuestionData | null> => {
+  await wait(500);
+  const results = questions.filter((q) => q.questionId === questionId);
+  return results.length === 0 ? null : results[0];
 };
 
 const wait = async (ms: number): Promise<void> => {
